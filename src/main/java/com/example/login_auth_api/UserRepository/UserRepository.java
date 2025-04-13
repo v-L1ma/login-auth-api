fe6d77927d0a0abf5b88afe18eb7fe74a550749c
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-13 14:41:01 -0300

    finalizando a api

--- src/main/java/com/example/login_auth_api/UserRepository/UserRepository.java
+++ src/main/java/com/example/login_auth_api/UserRepository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.login_auth_api.UserRepository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.login_auth_api.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-
+    Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/com/example/login_auth_api/dto/LoginRequestDto.java
@@ -0,0 +1,3 @@
+package com.example.login_auth_api.dto;
+
+public record LoginRequestDto (String email, String password) {}
